eebf6213138ed6a7aa187b5827a52ac8cb5d6feb
debuggerx01 <dx8917312@163.com>
2023-01-07 01:44:26 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -7,7 +7,7 @@ sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
 apt update
-apt install -y wget xz-utils make gcc flex bison dpkg
+apt install -y wget xz-utils make gcc flex bison dpkg-dev
 apt build-dep -y linux
 
 # change dir to workplace
