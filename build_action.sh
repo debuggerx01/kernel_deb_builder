beda0312bd1082a588ccd76960247f750a0e78b2
debuggerx01 <dx8917312@163.com>
2023-01-07 01:26:54 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -7,7 +7,7 @@ sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
 apt update
-apt install -y wget zx-utils
+apt install -y wget xz-utils
 apt build-dep -y linux
 
 # change dir to workplace
