9a6e457c979cb4e6afd3277383b4a5e19085e54a
debuggerx01 <dx8917312@163.com>
2023-01-07 02:20:18 +0800

    Update redmi_book_air_13_acpi.sh

--- patch.d/redmi_book_air_13_acpi.sh
+++ patch.d/redmi_book_air_13_acpi.sh
@@ -4,4 +4,4 @@
 sed -i "/ACPI_MAX_LOOP_TIMEOUT/s/30/3/g" include/acpi/acconfig.h
 
 # make kernel version to 5.xx.0
-sed -i '/SUBLEVEL =/s/[0-9]./0/g' Makefile
+sed -i '/SUBLEVEL =/s/[0-9].*/0/g' Makefile
